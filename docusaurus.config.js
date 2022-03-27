4208bba1cd4a2c12e391e7cf4170e4b32dec965e
renhongl <1075220132@qq.com>
2022-03-27 15:06:19 +0800

    1 file modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://renhongl.github.io',
-  baseUrl: '/',
+  baseUrl: '/pomelo/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
