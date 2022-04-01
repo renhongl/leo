609e328ec10811ac730af86e825bcd81b60006df
renhongl <1075220132@qq.com>
2022-04-02 01:23:45 +0800

    1 file modified

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,28 +2,28 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import Resume from '@site/src/components/Resume';
 
-function addScript(url) {
-  var script = document.createElement('script');
-  script.type = 'application/javascript';
-  script.src = url;
-  document.head.appendChild(script);
-}
-addScript(
-  'https://raw.githack.com/eKoopmans/html2pdf/master/dist/html2pdf.bundle.js'
-);
+// function addScript(url) {
+//   var script = document.createElement('script');
+//   script.type = 'application/javascript';
+//   script.src = url;
+//   document.head.appendChild(script);
+// }
+// addScript(
+//   'https://raw.githack.com/eKoopmans/html2pdf/master/dist/html2pdf.bundle.js'
+// );
 
 export default function Home() {
-  const downloadPdf = () => {
-    var element = document.getElementById('resume');
-    var opt = {
-      margin: 1,
-      filename: 'myfile.pdf',
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
-    };
-    html2pdf().set(opt).from(element).save();
-  };
+  // const downloadPdf = () => {
+  //   var element = document.getElementById('resume');
+  //   var opt = {
+  //     margin: 1,
+  //     filename: 'myfile.pdf',
+  //     image: { type: 'jpeg', quality: 0.98 },
+  //     html2canvas: { scale: 2 },
+  //     jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+  //   };
+  //   html2pdf().set(opt).from(element).save();
+  // };
 
   return (
     <Layout
