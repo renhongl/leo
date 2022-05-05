1b9a25ed2e38892c8664763925ccae698be4f207
renhongl <1075220132@qq.com>
2022-05-06 00:12:04 +0800

    6 files modified

--- src/pages/home-components/Window/index.js
+++ src/pages/home-components/Window/index.js
@@ -4,59 +4,39 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import Time from '../Time';
 import ReactDOM from 'react-dom';
 import Draggable from 'react-draggable';
+import { Resizable, ResizableBox } from 'react-resizable';
 
 export default function Window() {
-  const [size, setSize] = useState(['80%', '80%']);
-  const [expanded, setExpand] = useState(false);
-
-  useEffect(() => {
-    // document.addEventListener('mouseup', mouseUp);
-  }, []);
-
-  function onDoubleClick() {
-    if (expanded) {
-      setSize(['80%', '80%']);
-      setExpand(false);
-    } else {
-      setSize(['100%', '100%']);
-      setExpand(true);
-    }
-  }
-
   return (
     <Draggable
-      axis="x"
-      handle=".winTit"
+      axis="both"
+      handle="#win"
       defaultPosition={{ x: 0, y: 0 }}
       position={null}
-      grid={[25, 25]}
       scale={1}
     >
-      <section
-        className={styles.win}
-        style={{
-          width: size[0],
-          height: size[1],
-        }}
+      <ResizableBox
+        width={400}
+        height={400}
+        minConstraints={[100, 100]}
+        maxConstraints={[Infinity, Infinity]}
       >
-        <div
-          handle=".handle"
-          className={styles.winTit}
-          onDoubleClick={() => onDoubleClick()}
-        >
-          <span className={styles.red}></span>
-          <span className={styles.yellow}></span>
-          <span className={styles.gray}></span>
-        </div>
-        <div className={styles.winContent}>
-          <iframe
-            width="100%"
-            height="100%"
-            src="/leo"
-            frameBorder="0"
-          ></iframe>
-        </div>
-      </section>
+        <section className={styles.win}>
+          <div id="win" className={styles.winTit}>
+            <span className={styles.red}></span>
+            <span className={styles.yellow}></span>
+            <span className={styles.gray}></span>
+          </div>
+          <div className={styles.winContent}>
+            <iframe
+              width="100%"
+              height="100%"
+              src="/leo"
+              frameBorder="0"
+            ></iframe>
+          </div>
+        </section>
+      </ResizableBox>
     </Draggable>
   );
 }
