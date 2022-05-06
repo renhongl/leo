d4a9f4ee81c1e2b8deb71c63a7d71eddf47803af
renhongl <1075220132@qq.com>
2022-05-06 11:43:13 +0800

    1 file modified

--- src/pages/home-components/Docker/index.js
+++ src/pages/home-components/Docker/index.js
@@ -3,18 +3,23 @@ import styles from './index.module.css';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import Time from '../Time';
 
-export default function Docker({ data=[], toggleWin }) {
+export default function Docker({ data = [], toggleWin }) {
   return (
     <section className={styles.docker}>
       {data.map((item) => (
-        <div
-          onClick={() => toggleWin(item.id)}
-          key={item.id}
-          className={styles.item}
-          style={{
-            backgroundImage: 'url(' + useBaseUrl('/img/' + item.icon) + ')',
-          }}
-        ></div>
+        <div className={styles.itemWrap}>
+          <div
+            onClick={() => toggleWin(item.id)}
+            key={item.id}
+            className={styles.item}
+            style={{
+              backgroundImage: 'url(' + useBaseUrl('/img/' + item.icon) + ')',
+            }}
+          >
+            
+          </div>
+          {item.opened ? <span className={styles.opened}></span> : null}
+        </div>
       ))}
     </section>
   );
